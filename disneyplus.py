2fe838a6cc0e2293ce10426a353ab830fae2f64b
maxwellmg <maxmillergolub@gmail.com>
2021-02-11 16:25:42 -0500

    mod. disney xml extract/clean. needs export loc.

--- disneyplus.py
+++ disneyplus.py
@@ -1,24 +1,27 @@
-#disneyplus.py
+#disneyplus
 import sys
 from datetime import date
 import logging
 import requests
 import xml.etree.ElementTree as ET
-import pysitemap
 from pysitemap import crawler
-
 if __name__ == '__main__':
+	url_list = []
+	response = requests.get('https://cde-lumiere-disneyplus.bamgrid.com/d-sitemap-1.xml')
+	tree = ET.ElementTree(ET.fromstring(response.content))
+	root = tree.getroot()
+	for child in root:
+		url_list.append(child[0].text)
+	today=date.today()
+	d=today.strftime("%m.%d.%y")
+	with open(f'outputs/disneyplus{d}.txt', "w") as outfile:
+		for i in url_list:
+			if "https://www.disneyplus.com/movies/" in i:
+				outfile.write('%s\n' % i)
+			elif "https://www.disneyplus.com/series/" in i:
+				outfile.write('%s\n' % i)
+			else:
+				pass
 
-    url_list = []
-
-    tree = ET.parse('local_sitemaps/d-sitemap-1.xml')
-    root = tree.getroot()
-
-    for child in root:
-        url_list.append(child[0].text)
-
-    today=date.today()
-    d=today.strftime("%m.%d.%y")
-    with open(f'outputs/disneyplus{d}.txt', "w") as outfile:
-        for i in url_list:
-            outfile.write('%s\n' % i)
+# outfile is filled with good links only
+# code to cross reference db now?
