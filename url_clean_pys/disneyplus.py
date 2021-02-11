ec3d839157292a22f4c0d353d13b40905f370f28
maxwellmg <maxmillergolub@gmail.com>
2021-02-11 16:24:38 -0500

    disney url clean with advertools (outdated)

--- url_clean_pys/disneyplus.py
+++ url_clean_pys/disneyplus.py
@@ -2,14 +2,31 @@
 
 #Do we need all of the various country designated titles? or will movies/series be enough
 
-#open url output file
+import advertools
+#import pandas
+#import dataframe
 
-good_urls = []
+disney_sitemap = advertools.sitemap_to_df("https://cde-lumiere-disneyplus.bamgrid.com/d-sitemap-1.xml")
+for row in disney_sitemap['loc']:
+	good_urls = []
+	if "https://www.disneyplus.com/movies/" in row:
+		good_urls = good_urls.append(row)
+		print(row)
+	elif "https://www.disneyplus.com/series/" in row:
+		good_urls = good_urls.append(row)
+		print(row)
+	else:
+		pass
+print(good_urls)
+
+
+
+'''good_urls = []
 
 for url in txtfile:
     if "https://www.disneyplus.com/movies/" or "https://www.disneyplus.com/series/" in url:
         good_urls.append(url)
     else:
-        pass
+        pass'''
 
 #write urls to new outfile
