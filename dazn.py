b2358f3b0004326496abac03ca79e19855a62a6c
maxwellmg <maxmillergolub@gmail.com>
2021-02-16 13:05:38 -0500

    dazn urlgen ready, except for proper outfile loc

--- dazn.py
+++ dazn.py
@@ -1,23 +1,30 @@
-#disneyplus.py
+#dazn
 import sys
 from datetime import date
 import logging
 import requests
 import xml.etree.ElementTree as ET
-# from .. import pysitemap
+from pysitemap import crawler
 
 if __name__ == '__main__':
-
     url_list = []
-
-    tree = ET.parse('local_sitemaps/daznsitemap.xml')
+    response = requests.get('https://www.dazn.com/en-US/sitemap.xml')
+    tree = ET.ElementTree(ET.fromstring(response.content))
     root = tree.getroot()
-
     for child in root:
         url_list.append(child[0].text)
-
     today=date.today()
     d=today.strftime("%m.%d.%y")
-    with open(f'../outputs/dazn{d}.txt', "w") as outfile:
+    with open(f'outputs/dazn{d}.txt', "w") as outfile:
         for i in url_list:
-            outfile.write('%s\n' % i)
+            if ("competitor" in i) or ("sport"  in i) or ("schedule" in i):
+                pass
+            elif 'https://www.dazn.com/en-US/home' == i:
+                print('here')
+                pass
+            else:
+                outfile.write('%s\n' % i)
+
+
+
+# clean based on assumption that we don't want these types of urls ('https://www.dazn.com/en-US/competitor/Competitor:ipenlq6r1ae65q69twfd6ukr', 'https://www.dazn.com/en-US/sport/Sport:25z21evc6o0aewxnhyu2dzyrc', 'https://www.dazn.com/en-US/schedule')
